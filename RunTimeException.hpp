f7ec1f9fcc5fc46262358855d73afde0145aa2ba
DedovKonstantin <kostya_dedov@mail.ru>
2016-04-19 19:34:49 +0600

    Заголовочный файл с классом исключения.
    Заголовочный файл с классом исключения времени выполнения.

---
+++ RunTimeException.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <MyLib\MyString.hpp>
+#include <vector>
+
+using namespace std;
+
+class RunTimeException
+{
+private:
+	MyString message;
+public:
+	RunTimeException()
+	{
+	}
+	RunTimeException(const char *mes)
+	{
+		message = mes;
+	}
+	RunTimeException(const RunTimeException& other)
+	{
+		message = other.message;
+	}
+	~RunTimeException()
+	{
+	}
+	friend ostream& operator<<(ostream& os, const RunTimeException& exc)
+	{
+		os << exc.message;
+		return os;
+	}
+	RunTimeException& operator<<(const MyString& ms)
+	{
+		for (size_t i = 0; i < ms.getLength(); ++i)
+			message += ms[i];
+		return *this;
+	}
+	RunTimeException& operator<<(int num)
+	{
+		if (num < 0)
+			message += '-';
+		if (num == 0)
+		{
+			message += '0';
+			return *this;
+		}
+		vector<int> number;
+		while (num)
+		{
+			number.push_back(num % 10);
+			num /= 10;
+		}
+		for (size_t i = 0; i < number.size(); ++i)
+			message += number[number.size() - i - 1] + '0';
+		return *this;
+	}
+};
